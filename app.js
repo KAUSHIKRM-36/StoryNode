8581b8ec514df7f6ea53246c5a57bf0d7cd53492
KAUSHIKRM-36 <kaushikmahajan36@gmail.com>
2026-03-16 12:10:35 +0530

    fix railway root route

--- app.js
+++ app.js
@@ -523,6 +523,6 @@ app.post('/like/:postId', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
